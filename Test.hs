6d8053db923eed59c4c29ee4cd7996e6c9a56fa5
Tom Busby <tom@busby.ninja>
2014-10-11 20:27:46 +0100

    tidy up and comment one of the tests in Tests.hs

--- Test.hs
+++ Test.hs
@@ -28,15 +28,24 @@ randomBinaryBlock = do
 
 testThatApplyingInitialPermutationThenFinalPermutationReturnsTheInputData :: IO ()
 testThatApplyingInitialPermutationThenFinalPermutationReturnsTheInputData = do
+	-- Show test name
+	putStr "testThatApplyingInitialPermutationThenFinalPermutationReturnsTheInputData\n\n"
+
+	-- Do assertion with fixed data
 	b <- return constantBinaryBlock
 	b' <- return $ finalPermutation . initialPermutation $ b
-	c <- randomBinaryBlock
-	c' <- return $ finalPermutation . initialPermutation $ c
 	assertb <- return $ b == b'
-	assertc <- return $ c == c'
-	putStr "testThatApplyingInitialPermutationThenFinalPermutationReturnsTheInputData\n\n"
+
+	-- Show assert
 	putStr "Test that 170*8 btye block returns itself with inital then final permute => "
 	putStrLn $ show assertb
+	
+	-- Do assertion with random data
+	c <- randomBinaryBlock
+	c' <- return $ finalPermutation . initialPermutation $ c
+	assertc <- return $ c == c'
+
+	-- Show assert
 	putStr "Test that random byte block returns itself with inital then final permute => "
 	putStrLn $ show assertc
 	putStr "\n"
