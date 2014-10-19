a7ac0f7831cb6b0734ef836891c083a455f0fc29
Tom Busby <tom@busby.ninja>
2014-10-19 22:03:13 +0100

    read in the key from file and handle the exception if the file is not found

--- round/Encrypt.hs
+++ round/Encrypt.hs
@@ -1,7 +1,23 @@
 module Encrypt (performEncrypt) where
 
 import Global(Error, Rounds, KeyFilename, InputFilename, OutputFilename)
+import Control.Exception(SomeException, try)
+import Data.ByteString(ByteString, unpack)
+import qualified Data.ByteString as BS(readFile)
+import Data.ByteString.Base64(decodeLenient)
+
+import DebugDisplay(toBinaryRepresentation) -- REMOVE LATER
 
 performEncrypt :: KeyFilename -> InputFilename -> OutputFilename -> IO ()
 performEncrypt k i o = do
-	putStrLn "Performing encryption"
+	maybeKey <- getKeyFromFile k
+	case maybeKey of
+		Nothing -> putStrLn $ "We were unable to find and/or read from a key at " ++ k
+		Just key -> putStrLn "Key read from file and decoded from Base64"
+
+getKeyFromFile :: KeyFilename -> IO (Maybe ByteString)
+getKeyFromFile keyFilename = do
+	fileRead <- try (BS.readFile keyFilename) :: IO (Either SomeException ByteString)
+	case fileRead of
+		Left _ -> return Nothing
+		Right key -> return . Just . decodeLenient $ key
